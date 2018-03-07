b35e02e88a484b488f2d5bbdaa403cb32a03874b
fz <1404531344@qq.com>
2018-03-07 13:49:29 +0800

    b

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("我只想一个抽着烟，看着夜，从此这世间少了一份留恋，多了些孤独与固执。。。。");
         System.out.println("fz,holler");
+        System.out.println("fz,123");
     }
 }
